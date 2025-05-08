6892df3e5f81a9c610b6de0a8b8d06ee1bc69b7c
arnauvidal@gmail.com <arnauvidal@gmail.com>
2025-05-08 10:37:39 +0200

    add working code to main.c file

--- main.c
+++ main.c
@@ -1,57 +1,84 @@
-// Write your code in this file
 #include <stdio.h>
 #include <string.h>
 
-// #define acts like a global "find replace"
-#define STUDENT_NUM 5
-#define EXAMS_NUM 13
-#define MAX_NAME_LEN 11
-
-struct Student
-{
-    char name[MAX_NAME_LEN];
-    int exam[EXAMS_NUM];
-    float average;
-};
+#define NUM_STUDENTS 5
+#define NUM_GRADES 13
 
 int main()
 {
-    struct Student students[STUDENT_NUM];
-    char input[100];
+    char names[NUM_STUDENTS][50];
+    int grades[NUM_STUDENTS][NUM_GRADES];
+    float averages[NUM_STUDENTS];
+    int sum_grades;
 
-    printf("Add student data, (name followed bt 13 examscores): \n");
-    for (int i = 0; i < STUDENT_NUM; i++)
+    printf("Provide the information of %d students.(name followed by 13 grades): \n", NUM_STUDENTS);
+
+    for (int s = 0; s < NUM_STUDENTS; s++)
     {
-        printf("Student %d: ", i + 1);
+        // Store the names
+        printf("Student %d: ", s + 1);
+        scanf("%s", names[s]);
+        // Capitalize names
+        if (names[s][0] >= 'a' && names[s][0] <= 'z')
+        {
+            names[s][0] = names[s][0] - 32; //-32 to capitalize it
+        }
 
-        // Reads the entire line include spaces
-        fgets(input, sizeof(input), stdin);
+        // Store the grades
+        sum_grades = 0; // reset it
+        for (int g = 0; g < NUM_GRADES; g++)
+        {
+            scanf("%d", &grades[s][g]);
+            sum_grades += grades[s][g];
+        }
 
-        // Parse the input using sscanf
-        int parsed = sscanf(input, "%10s, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d",
-                            students[i].name,
-                            students[i].exam[0],
-                            students[i].exam[1],
-                            students[i].exam[2],
-                            students[i].exam[3],
-                            students[i].exam[4],
-                            students[i].exam[5],
-                            students[i].exam[6],
-                            students[i].exam[7],
-                            students[i].exam[8],
-                            students[i].exam[9],
-                            students[i].exam[10],
-                            students[i].exam[11],
-                            students[i].exam[12]
-
-        );
+        // Calculates average
+        averages[s] = (float)sum_grades / NUM_GRADES;
     }
 
-    for (int i = 0; i < STUDENT_NUM; i++)
-    {
-        printf("%s, ", students[i].name);
-    }
     printf("\n");
+    // Print results
+    for (int s = 0; s < NUM_STUDENTS; s++)
+    {
+        printf("%s Grades: ", names[s]);
+        for (int i = 0; i < NUM_GRADES; i++)
+        {
+            printf("%d, ", grades[s][i]);
+        }
+        printf("Average: %.001f \n", averages[s]);
+    }
+
+    // Find the student with the highest average
+    float highest_average = averages[0];
+    int top_student_index = 0;
+
+    for (int s = 1; s < NUM_STUDENTS; s++) // start at 1 since we assume 0 is highest so far
+    {
+        if (averages[s] > highest_average)
+        {
+            highest_average = averages[s];
+            top_student_index = s;
+        }
+    }
+
+    // Print top student
+    printf("\n");
+    printf("%s\n", names[top_student_index]);
+
+    // Find students with average under 5
+    float average_under_media = averages[0];
+    int students_under_media = 0;
+
+    printf("\n");
+    for (int s = 0; s < NUM_STUDENTS; s++)
+    {
+        if (averages[s] <= 5)
+        {
+            average_under_media = averages[s];
+            students_under_media = s;
+            printf("%s \n", names[students_under_media]);
+        }
+    }
 
     return 0;
 }
