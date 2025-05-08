86c39aa41767df83335c3a0989888082c0bc43db
arnauvidal@gmail.com <arnauvidal@gmail.com>
2025-05-08 10:36:03 +0200

    test.c find student with average = or under media working!

--- test.c
+++ test.c
@@ -11,7 +11,7 @@ int main()
     float averages[NUM_STUDENTS];
     int sum_grades;
 
-    printf("Provide the information of %d students. One student a time (name followed by 13 grades): \n", NUM_STUDENTS);
+    printf("Provide the information of %d students.(name followed by 13 grades): \n", NUM_STUDENTS);
 
     for (int s = 0; s < NUM_STUDENTS; s++)
     {
@@ -25,6 +25,7 @@ int main()
         }
 
         // Store the grades
+        sum_grades = 0; // reset it
         for (int g = 0; g < NUM_GRADES; g++)
         {
             scanf("%d", &grades[s][g]);
@@ -35,16 +36,48 @@ int main()
         averages[s] = (float)sum_grades / NUM_GRADES;
     }
 
+    printf("\n");
     // Print results
     for (int s = 0; s < NUM_STUDENTS; s++)
     {
-        printf("\nName: %s\nGrades: ", names[s]);
+        printf("%s Grades: ", names[s]);
         for (int i = 0; i < NUM_GRADES; i++)
         {
             printf("%d, ", grades[s][i]);
         }
-        printf("\n");
-        printf("%.001f ", averages[s]);
+        printf("Average: %.001f \n", averages[s]);
+    }
+
+    // Find the student with the highest average
+    float highest_average = averages[0];
+    int top_student_index = 0;
+
+    for (int s = 1; s < NUM_STUDENTS; s++) // start at 1 since we assume 0 is highest so far
+    {
+        if (averages[s] > highest_average)
+        {
+            highest_average = averages[s];
+            top_student_index = s;
+        }
+    }
+
+    // Print top student
+    printf("\n");
+    printf("%s\n", names[top_student_index]);
+
+    // Find students with average under 5
+    float average_under_media = averages[0];
+    int students_under_media = 0;
+
+    printf("\n");
+    for (int s = 0; s < NUM_STUDENTS; s++)
+    {
+        if (averages[s] <= 5)
+        {
+            average_under_media = averages[s];
+            students_under_media = s;
+            printf("%s \n", names[students_under_media]);
+        }
     }
 
     return 0;
